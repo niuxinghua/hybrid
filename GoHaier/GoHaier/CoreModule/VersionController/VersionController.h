ecde05a21d3da27e972d2407305479460e3c3497
niuxinghua <niuxinghua@haier.com>
2018-12-26 16:52:33 +0800

    no message
    Change-Id: I3d82fc931bd26730301463a6d7221958538a4cda

---
+++ GoHaier/GoHaier/CoreModule/VersionController/VersionController.h
@@ -0,0 +1,17 @@
+//
+//  VersionController.h
+//  GoHaier
+//
+//  Created by niuxinghua on 2018/12/26.
+//  Copyright © 2018年 com.haier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VersionController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GoHaier/GoHaier/CoreModule/VersionController/VersionController.m
@@ -0,0 +1,13 @@
+//
+//  VersionController.m
+//  GoHaier
+//
+//  Created by niuxinghua on 2018/12/26.
+//  Copyright © 2018年 com.haier. All rights reserved.
+//
+
+#import "VersionController.h"
+
+@implementation VersionController
+
+@end
